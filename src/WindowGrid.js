34dd44027f006ba505dafd6ad35c03a875300e15
Noah Kaplan <nkaplan1@uncc.edu>
2018-12-22 22:08:25 -0500

    stopped using the draggable import, created my own dragging implementation

--- src/WindowGrid.js
+++ src/WindowGrid.js
@@ -6,7 +6,6 @@ export default class WindowGrid extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            currentTierGridRow : 0,
             inSelectionGrid : ["drmario","mario","luigi","bowser","peach","yoshi","dk","falcon","ganon",
             "falco","fox","ness","iceclimbers","kirby","samus","sheik","link","younglink",
             "pichu","pikachu","jigglypuff","mewtwo","gnw","marth","roy"],
@@ -14,7 +13,7 @@ export default class WindowGrid extends React.Component{
         };
     };
 
-    moveCharacters(currentCharacter){        
+    moveCharacters(currentCharacter,currentRow){        
         let newInSelectionGrid = this.state.inSelectionGrid;
         let newInTierListGrid = this.state.inTierListGrid;
 
@@ -22,38 +21,35 @@ export default class WindowGrid extends React.Component{
 
         newInTierListGrid = this.state.inTierListGrid.filter( pair => pair[0] !== currentCharacter);
         
-        if(this.state.currentTierGridRow === 0){
+        if(currentRow === 7){
             newInSelectionGrid.push(currentCharacter);
         }
         else{
-            newInTierListGrid.push([currentCharacter,this.state.currentTierGridRow-1]);
+            newInTierListGrid.push([currentCharacter,currentRow-1]);
         }
         this.setState({
             inSelectionGrid: newInSelectionGrid,
             inTierListGrid: newInTierListGrid,
         });
     }
-    onMouseOver(tierGridRow){
-        this.setState({
-            currentTierGridRow: tierGridRow
-        });
-    }
-    handleStop(name){
-        this.moveCharacters(name);
+    onDrop(ev, row){
+        this.render();
+        let name = ev.dataTransfer.getData("name");
+        console.log('Dropped Row:', row);
+        this.moveCharacters(name,row);
     }
 
     render(){
         return(
             <div className = "mainGridWrapper">
-                <div className = "rightSideMenu">Right Side Menu char:{this.state.currentCharacter} row:{this.state.currentTierGridRow}</div>
+                <div className = "rightSideMenu"></div>
                 <div className = "contentBody">
                     <TierListChart 
                         characterList = {this.state.inTierListGrid} 
-                        onMouseOver = {(i) => this.onMouseOver(i)} 
-                        onDragStop = {(i) => this.handleStop(i)}/>
+                        onDrop = {(ev,row) => this.onDrop(ev,row)}/>
                     <CharacterSelectionGrid 
                         characterList = {this.state.inSelectionGrid}
-                        onDragStop = {(i) => this.handleStop(i)}
+                        onDrop = {(ev,row) => this.onDrop(ev,row)}
                         /*onMouseOver = {() => this.onMouseOver(0)}*//>
                 </div>
             </div>
