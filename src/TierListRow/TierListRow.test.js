55a7a276061b2e1e8c974c005d0e702c2375738b
Noah Kaplan <nkaplan1@uncc.edu>
2019-01-01 20:10:29 -0500

    removed dependency on the character card element on the Tier List row tests

--- src/TierListRow/TierListRow.test.js
+++ src/TierListRow/TierListRow.test.js
@@ -1,19 +1,16 @@
 import React from "react"
 import TierListRow from "./TierListRow"
-import CharacterCard from "../CharacterCard/CharacterCard"
 import { render } from "react-testing-library";
 
 describe("Tier List Row" , function(){
     it("renders without crashing", function(){
-        let character = <CharacterCard name = "yoshi"/>;
-        render(<TierListRow characters = {character}/>);
+    let element = [<div key = "placeholder">placeholder</div>];
+        render(<TierListRow characters = {element}/>);
     });
     it("renders the correct icons", function(){
-        let characters = [<CharacterCard key = "yoshi" name = "yoshi"/>, <CharacterCard key = "bowser" name = "bowser"/>];
-        const {getByAltText} = render(<TierListRow characters = {characters}></TierListRow>);
-        const yoshi = getByAltText("yoshi");
-        const bowser = getByAltText("bowser");
-        expect(yoshi).toBeDefined();
-        expect(bowser).toBeDefined();
+        let elements = [<div key = "placeholder">placeholder</div>];
+        const {getByText} = render(<TierListRow characters = {elements}></TierListRow>);
+        const placeholderElement = getByText("placeholder");
+        expect(placeholderElement).toBeDefined();
     });
 });
