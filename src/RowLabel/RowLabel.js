1c320103a97e515db95b41e97e3c9cc6e1e4854a
Noah Kaplan <nkaplan1@uncc.edu>
2019-01-06 10:08:29 -0500

    created icon on tier list label for expanding a context menu in the future

--- src/RowLabel/RowLabel.js
+++ src/RowLabel/RowLabel.js
@@ -2,6 +2,9 @@ import React from "react"
 
 export default function RowLabel(props){
     return(
-        <textarea className = "rowLabel" placeholder = {props.placeholder}></textarea>
+        <div className = "rowLabelWrapper">
+            <button className = "expandButton">+</button>
+            <textarea className = "rowLabelText" placeholder = {props.placeholder}></textarea>
+        </div>
     );
 }
