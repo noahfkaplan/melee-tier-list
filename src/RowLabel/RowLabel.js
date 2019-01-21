15e6e971693f8479645cdebe66018f59bb83790c
Noah Kaplan <nkaplan1@uncc.edu>
2019-01-21 09:16:38 -0500

    improved coverage of Row Label tests

--- src/RowLabel/RowLabel.js
+++ src/RowLabel/RowLabel.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function RowLabel(props){
     return(
         <div className = "rowLabelWrapper">
-            <button className = "expandButton" onClick ={(event)=>props.onClick(event)}>...</button>
+            <button className = "expandButton" data-testid = "expand-button" onClick ={(event)=>props.onClick(event)}>...</button>
             <textarea 
                 className = "rowLabelText" 
                 spellCheck = "false" 
