5bd37a3b6d6325029b56e276d6559f9cc5d57202
Noah Kaplan <nkaplan1@uncc.edu>
2019-03-24 08:51:11 -0400

    created integration test project for testing apis

---
+++ MeleeTierListServer/MeleeTierListServer.IntegrationTests/Controllers/LoadControllerTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MeleeTierListServer.Controllers;
+
+namespace MeleeTierListServer.IntegrationTests.Controllers
+{
+    [TestClass]
+    public class LoadControllerTests
+    {
+        [TestMethod]
+        public void LoadControllerGet_WhenCalledWithGuid_ReturnsStatusCodeOK()
+        {
+            //Arrange
+            var loadController = new LoadController();
+            //Act
+            var response = loadController.Get(System.Guid.NewGuid().ToString());
+            //Assert
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
