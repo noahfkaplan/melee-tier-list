89b7ab649a773d743934fc9e8318ac1d78faff75
Noah Kaplan <nkaplan1@uncc.edu>
2019-05-18 12:03:27 -0400

    removed database connections and simplified load api so that I can get things integrated before worrying about database stuff

--- MeleeTierListServer/MeleeTierListServer/Controllers/LoadController.cs
+++ MeleeTierListServer/MeleeTierListServer/Controllers/LoadController.cs
@@ -4,6 +4,8 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using MeleeTierListServer.Models;
+using MeleeTierListServer.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MeleeTierListServer.Controllers
@@ -11,38 +13,17 @@ namespace MeleeTierListServer.Controllers
     [Route("api/[controller]")]
     public class LoadController : Controller
     {
-        // GET api/values/5
-        [HttpGet("{TierListId}")]
-        public HttpResponseMessage Get(string id)
+        private CharacterRepository characterRepository;
+        public LoadController()
         {
-            //Guid tableID = new Guid(id);
-            Guid tableID = new Guid("90B0E383-FE33-404C-8968-426570C87945");
-            string connectionString = "Server=WINDOWS-4F0KJMT;Database=MeleeTierListQA;Integrated Security=true;";
-            string query = ($"SELECT RowNumber, CharacterList FROM dbo.TierListRows WHERE TierListID = '{tableID}'");
-            string characterList = "";
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand command = new SqlCommand(query, connection);
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                try
-                {
-                    while (reader.Read())
-                    {
-                        string temp = String.Format("{0}, {1}\n",
-                            reader[0], reader[1]);
-                        characterList += temp;
-                    }
-                }
-                finally
-                {
-                    // Always call Close when done reading.
-                    reader.Close();
-                }
-            }
-            var response = new HttpResponseMessage();
-            response.Content = new StringContent(characterList);
-            return response;
+            characterRepository = new CharacterRepository();
+        }
+
+        // GET api/load/5
+        [HttpGet("{TierListId}")]
+        public Character[] Get(string id)
+        {
+            return characterRepository.GetCharactersByTierListID(id);
         }
     }
 }
---
+++ MeleeTierListServer/MeleeTierListServer/Services/CharacterRepository.cs
@@ -0,0 +1,54 @@
+﻿using MeleeTierListServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeleeTierListServer.Services
+{
+    public class CharacterRepository
+    {
+        public Character[] GetCharactersByTierListID(string tierListID)
+        {
+
+            return new Character[]
+            {
+                new Character
+                {
+                    Name = "fox",
+                    Tier = 1,
+                },
+                new Character
+                {
+                    Name = "falco",
+                    Tier = 2,
+                },
+                new Character
+                {
+                    Name = "peach",
+                    Tier = 3,
+                },
+                new Character
+                {
+                    Name = "yoshi",
+                    Tier = 4,
+                },
+                new Character
+                {
+                    Name = "mario",
+                    Tier = 5,
+                },
+                new Character
+                {
+                    Name = "link",
+                    Tier = 6,
+                },
+                new Character
+                {
+                    Name = "bowser",
+                    Tier = 7,
+                }
+            };
+        }
+    }
+}
